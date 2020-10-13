b46bab12b6f3e5e88000558c9c25f9cf2d3a3172
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2020-10-13 21:18:32 +0300

    gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ let sass = require('gulp-sass');
 gulp.task('sass', function(){
   return gulp.src('app/scss/*.scss')
   .pipe(sass()) // compile SCSS to CSS
-  .pipe(gulp.dest('dist')); // put final CSS in dist folder
+  .pipe(gulp.dest('docs')); // put final CSS in dist folder
 });
 
 gulp.task('watch', function(){
